abc58ab1b10b69b98f65d39f6748347a3c155e8d
Branko <brankorakonjac036@gmail.com>
2022-06-06 22:46:43 +0200

    finished administrator

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
+import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
+
 
 @Injectable()
 export class AdministratorService {
@@ -10,14 +14,60 @@ export class AdministratorService {
         private readonly administrator: Repository<Administrator>,
     ){}
 
-    getAll(): Promise<Administrator[]>{
+  getAll(): Promise<Administrator[]>{
         return this.administrator.find();
     }
 
-    getById(id: number): Promise<Administrator>{
+  getById(id: number): Promise<Administrator>{
        return this.administrator.findOne({ 
         where: { 
           administratorId: id
         } 
       });
-    }}
+    }
+  add(data: AddAdministratorDto): Promise<Administrator | ApiResponse>{
+    
+    const crypto = require('crypto');
+    const passwordHash = crypto.createHash('sha512');
+    passwordHash.update(data.password);
+    const passwordHashString = passwordHash.digest('hex').toUpperCase();
+    
+    let newAdmin: Administrator= new Administrator();
+    newAdmin.username = data.username;
+    newAdmin.passwordHash = passwordHashString;
+
+    return new Promise((resolve) =>{
+      this.administrator.save(newAdmin)
+      .then(data => resolve(data))
+      .catch(error =>{
+        const response: ApiResponse = new ApiResponse("error", -1001);
+        resolve(response);
+      })
+    }) 
+  }
+
+  
+  async editById(id: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse>{
+    let admin: Administrator = await this.administrator.findOne({ 
+      where: { 
+        administratorId: id
+      } 
+    });
+
+    if( admin === undefined){
+      return new Promise((resolve) =>{
+        resolve(new ApiResponse( "error", -1002)); 
+      });
+    }
+
+  const crypto = require('crypto');
+  const passwordHash = crypto.createHash('sha512');
+  passwordHash.update(data.password);
+  const passwordHashString = passwordHash.digest('hex').toUpperCase();
+  admin.passwordHash = passwordHashString;
+    return this.administrator.save(admin); 
+ 
+  }
+
+
+  }
