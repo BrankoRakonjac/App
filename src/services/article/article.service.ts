375e460889824e3226d28566e9887d1dbdbccf95
Branko <brankorakonjac036@gmail.com>
2022-07-27 22:19:09 +0200

    Implement article editing features

--- src/services/article/article.service.ts
+++ src/services/article/article.service.ts
@@ -7,6 +7,7 @@ import { Article } from "src/entities/article.entity";
 import { AddArticleDto } from "src/dtos/article/add.article.dto";
 import { ApiResponse } from "src/misc/api.response.class";
 import { Repository } from "typeorm";
+import { EditArticleDto } from "src/dtos/article/edit.article.dto";
 
 
 @Injectable()
@@ -61,5 +62,68 @@ async createFullArticle(data: AddArticleDto): Promise< Article | ApiResponse>{
   });
     
 }
+
+    async editFullArticle(articleId: number, data: EditArticleDto): Promise< Article | ApiResponse>{
+        const existingArticle: Article = await this.article.findOne({
+            where: { 
+                articleId: articleId
+                },
+                relations: ['articlePrices', 'articleFeatures']
+        });
+
+        if(!existingArticle){
+            return new ApiResponse('error', -5001, 'Article not found');
+        }
+
+        existingArticle.name = data.name;
+        existingArticle.categoryId = data.categoryId;
+        existingArticle.excerpt = data.excerpt;
+        existingArticle.description = data.description;
+        existingArticle.status = data.status;
+        existingArticle.isPromoted = data.isPromoted;
+
+        const saveArticle = await this.article.save(existingArticle);
+        if(!saveArticle){
+            return new ApiResponse('error', -5002, 'Could not save new article data')
+        }
+
+        const newPriceString: string = Number(data.price).toFixed(2);
+        const lastPrice = existingArticle.articlePrices[existingArticle.articlePrices.length-1].price
+        const lastPriceString: string = Number(data.price).toFixed(2);
+
+        if(newPriceString !== lastPriceString){
+            const newArticlePrice = new ArticlePrice();
+            newArticlePrice.articleId = articleId;
+            newArticlePrice.price = data.price;
+
+            const saveArticlePrice = await this.articlePrice.save(newArticlePrice);
+            if(!saveArticlePrice){
+                return new ApiResponse('error', -5003, 'Could not save the new article prices')
+            }
+        }
+
+        if(data.features !== null){
+            await this.articleFeature.remove(existingArticle.articleFeatures);
+
+            for( let feature of data.features){
+                let newArticleFeature: ArticleFeature = new ArticleFeature();
+                newArticleFeature.articleId = articleId;
+                newArticleFeature.featureId = feature.featureId;
+                newArticleFeature.value = feature.value;
+        
+                await this.articleFeature.save(newArticleFeature);
+            }
+        }
+        return await this.article.findOne({ where: { 
+            articleId:articleId
+            } ,
+            relations: [
+                "category",
+                "articleFeatures",
+                "features",
+                "articlePrices"
+             ]
+       });
+    }
 
 }
